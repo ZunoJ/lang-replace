7140904c0bba95b9257aac3eeca93d8e3ddd237d
ming.zhu <ming.zhu@hstong.com>
2023-08-16 13:50:56 +0800

    feat: 修改读取新旧两份excel文档

--- bin/www.js
+++ bin/www.js
@@ -6,27 +6,45 @@ const path = require("path");
 const ExcelJS = require("exceljs");
 
 const projectPath = path.join(__dirname, "../../../"); // 前端项目目录
-const excelFilePath = `./${process.env.NAME || "doc"}.xlsx`; // Excel文件的路径，根据你的实际路径进行修改
-let keyIndex;
-let newKeyIndex;
+const excelFilePath = `./${process.env.NAME || "doc"}.xlsx`; // 新Excel文件的路径，根据你的实际路径进行修改
+const oldExcelFilePath = `./${process.env.OLDNAME || "oldDoc"}.xlsx`; // 旧Excel文件的路径，根据你的实际路径进行修改
+let sheetKeyIndex;
+let oldSheetKeyIndex;
+let sheetEnglishIndex;
+let oldSheetEnglishIndex;
 // 读取 Excel 文件
-async function readExcel(filePath) {
+async function readExcel(filePath, oldFilePath) {
   const workbook = new ExcelJS.Workbook();
+  const oldWorkbook = new ExcelJS.Workbook();
   await workbook.xlsx.readFile(filePath);
+  await oldWorkbook.xlsx.readFile(oldFilePath);
 
   const sheet = workbook.getWorksheet(1);
+  const oldSheet = oldWorkbook.getWorksheet(1);
   const keyMapping = {};
 
   sheet.eachRow((row, rowNumber) => {
     if (rowNumber !== 1) {
-      const key = row.getCell(keyIndex).value;
-      const newKey = row.getCell(newKeyIndex).value;
-      keyMapping[key] = newKey;
+      const key = row.getCell(sheetKeyIndex).value;
+      const englishText = row.getCell(sheetEnglishIndex).value;
+      oldSheet.eachRow((oldRow, oldRowNumber) => {
+        if (oldRowNumber !== 1) {
+          const oldKey = oldRow.getCell(oldSheetKeyIndex).value;
+          const oldenglishText = oldRow.getCell(oldSheetEnglishIndex).value;
+          if (englishText === oldenglishText) {
+            keyMapping[oldKey] = key;
+          }
+        } else {
+          oldSheetKeyIndex = oldRow.values.indexOf("key");
+          oldSheetEnglishIndex = oldRow.values.indexOf("英文");
+        }
+      });
     } else {
-      keyIndex = row.values.indexOf("key");
-      newKeyIndex = row.values.indexOf("newkey");
+      sheetKeyIndex = row.values.indexOf("key");
+      sheetEnglishIndex = row.values.indexOf("英文");
     }
   });
+  console.log(keyMapping);
   return keyMapping;
 }
 // 定义匹配$T方法调用的正则表达式
@@ -37,7 +55,7 @@ const files = glob.sync("**/*.+(vue|js)", {
   ignore: "**/node_modules/**", // 忽略node_modules文件夹
 });
 // 获取 key 映射
-readExcel(excelFilePath).then((res) => {
+readExcel(excelFilePath, oldExcelFilePath).then((res) => {
   // 遍历所有文件
   files.forEach((file) => {
     const content = fs.readFileSync(file, "utf-8");
